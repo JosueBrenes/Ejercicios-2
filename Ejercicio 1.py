02a42d5a0df4a41e668fa6096244501fba2c0251
Josue Brenes <josuebrenes3005@gmail.com>
2023-02-02 15:13:54 -0600

    Ejercicios

---
+++ Ejercicio 1.py
@@ -0,0 +1,12 @@
+# = Solicita la informacion de la persona
+print("Dgite el nombre de la persona")
+nombre = input()
+
+print("Digite su primer apellido: ")
+primerApellido = input()
+
+print("Digite su segundo apellido: ")
+segundoApellido = input()
+# = Imprime el mensaje de bienvenida
+
+print("Bienvenido, estimado (a) " + nombre + " " + primerApellido + " " + segundoApellido)
---
+++ Ejercicio 2.py
@@ -0,0 +1,24 @@
+# = Solicitamos los numeros que quiere usar
+print("Digite el numero A: ")
+primerNumero = input()
+primerNumero = int(primerNumero)
+
+print("Digite el numero B: ")
+segundoNumero = input()
+segundoNumero = int(segundoNumero)
+
+# = Se suman las variables e imprimimos el resultado
+suma = (primerNumero + segundoNumero)
+print("El resultado de la suma es: " + str(suma))
+
+# = Se restan las variables e imprimimos el resultado
+resta = (primerNumero + segundoNumero)
+print("El resultado de la resta es: " + str(resta))
+
+# = Se dividen las variables e imprimimos el resultado
+division = (primerNumero / segundoNumero)
+print("El resultado de la resta es: " + str(division))
+
+# = Se multiplican las variables e imprimimos el resultado
+multiplicacion = (primerNumero * segundoNumero)
+print("El resultado de la multiplicacion es de: " + str(multiplicacion))
